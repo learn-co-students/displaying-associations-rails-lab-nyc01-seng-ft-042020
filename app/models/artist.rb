6f5044a2d4cc2ca875b9cadb3e79c5db69c94411
rwilt <rosie.wilt@gmail.com>
2020-05-18 20:47:15 -0400

    Done.

--- app/models/artist.rb
+++ app/models/artist.rb
@@ -1,2 +1,9 @@
 class Artist < ActiveRecord::Base
+    has_many :songs
+
+    def song_count
+        self.songs.each do |song|
+            song
+        end.count
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+
+a1 = Artist.create(name:"Beyonce")
+a2 = Artist.create(name:"Drake")
+a3 = Artist.create(name:"David Bowie")
+Song.create(title: "We Can Be Heroes", artist:a3)
+Song.create(title: "Naughty Girl", artist:a1)
+Song.create(title: "Deja Vu", artist:a1)
+Song.create(title: "Controlla", artist:a2)
+Song.create(title: "Hotline Bling", artist:a2)
+
+
+
